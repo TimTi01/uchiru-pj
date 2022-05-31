f9910ec8a6b6aa0863ba55be431e5a61ac0144eb
TimTi01 <TimF1234@yandex.ru>
2022-05-31 14:17:18 +0300

    added deploy

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import App from '../App'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { AllCatsPages } from '../Pages/AllCatsPages'
 import { FavoriteСats } from '../Pages/FavoriteСats'
 
 export const Routers = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/' element={<App/>}>
                 <Route index element={<AllCatsPages/>}/>
                 <Route path='favorite' element={<FavoriteСats/>}/>
             </Route>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
